206d71c839a5103420efd5a46f5d79db0a0c9d48
kessers <koen.essers@ru.nl>
2026-09-15 00:51:45 +0200

    fixed y

--- scripts/general_utils/m_dup.py
+++ scripts/general_utils/m_dup.py
@@ -461,7 +461,7 @@ def track_DUP(model):
 
         # ----------------------------------------------------------
         # accumulate
-        # ----------------------------------------------------------
+        # ----age------------------------------------------------------
 
         core_lost += dcore
 
@@ -793,6 +793,23 @@ class Abundances:
         self.initial_abundance = initial_abundance
 
     def __getattr__(self, name):
+
+        if name in ["zr93"]:
+            intershell = self.compute_intershell(name, isotope=True)
+            envelope = self.compute_envelope_abundance(name, intershell, initial=0)
+            envelope_decay = self.compute_envelope_abundance_decay(
+                name, intershell, initial=0
+            )
+
+            self.df.isotopes[name].envelope = envelope
+            self.df.isotopes[name].envelope_decay = envelope_decay
+            self.df.isotopes[name].intershell = intershell
+            if self.model != None:
+                self.df.isotopes[name].m_accreted = np.cumsum(envelope * self.dm_acc)
+            self.df.isotopes[name].m_yield = np.cumsum(envelope * self.dm)
+
+            return self.df.isotopes[name]
+
         if name in self.df.elements:
 
             # compute the intershell elemental abundance
@@ -994,7 +1011,7 @@ class Abundances:
 
         # self._prepare_single_monash_model(M, Z)
 
-    def compute_intershell(self, name):
+    def compute_intershell(self, name, isotope=False):
         # TODO: this needs to be changed to the ACTUAL abundances
         # keep in mind that we need to sum the abundances for the
         # different isotopes separately.
@@ -1014,6 +1031,15 @@ class Abundances:
                 index = np.where(self.m_dup > 10.0**-4.5)[0][0]
                 interp_x = self.tp_count - self.tp_count[index]
 
+        if isotope:
+            intershell += 93 * self.get_abundance(
+                self.df.isotopes["zr93"],
+                self.mass,
+                self.Z,
+                interp_x,
+            )
+            return intershell
+
         for isotope in self.df.elements[name].isotopes:
 
             intershell += self.df.elements[name].isotopes[
@@ -1025,17 +1051,17 @@ class Abundances:
                 interp_x,
             )
 
-            if name == "nb":
-                isos = self.df.elements["zr"].isotopes
-                intershell += 93 * self.get_abundance(
-                    self.df.isotopes["zr93"], self.mass, self.Z, interp_x
-                )
+        if name == "nb":
+            isos = self.df.elements["zr"].isotopes
+            intershell += 93 * self.get_abundance(
+                self.df.isotopes["zr93"], self.mass, self.Z, interp_x
+            )
 
-            if name == "zr":
-                isos = self.df.elements["zr"].isotopes
-                intershell -= 93 * self.get_abundance(
-                    self.df.isotopes["zr93"], self.mass, self.Z, interp_x
-                )
+        if name == "zr":
+            isos = self.df.elements["zr"].isotopes
+            intershell -= 93 * self.get_abundance(
+                self.df.isotopes["zr93"], self.mass, self.Z, interp_x
+            )
 
         return intershell
 
@@ -1090,7 +1116,7 @@ class Abundances:
 
         return abundance_min + weight * (abundance_max - abundance_min)
 
-    def compute_envelope_abundance(self, name, intershell):
+    def compute_envelope_abundance(self, name, intershell, initial=None):
         # INFO: gets the initial envelope abundance of the element
         # scaled by the metallicity of the model.
 
@@ -1102,13 +1128,15 @@ class Abundances:
         # )
 
         # INFO: THIS is the linearly interpolated method
-
-        if type(self.initial_abundance) == type(None):
-            initial_envelope_abundance = self.initial_envelope_abundances[
-                self.initial_envelope_abundances["element"] == name
-            ]["massfrac"]
+        if initial == None:
+            if type(self.initial_abundance) == type(None):
+                initial_envelope_abundance = self.initial_envelope_abundances[
+                    self.initial_envelope_abundances["element"] == name
+                ]["massfrac"]
+            else:
+                initial_envelope_abundance = self.initial_abundance
         else:
-            initial_envelope_abundance = self.initial_abundance
+            initial_envelope_abundance = initial
 
         # INFO: computes the elemental abundance in the envelope by
         # enriching it with intershell abundances.
@@ -1129,6 +1157,56 @@ class Abundances:
             envelope[i] = (
                 envelope[i - 1] * (self.m_env[i] - self.m_dup[i]) + delta_M_element[i]
             ) / self.m_env[i]
+
+        return envelope
+
+    def compute_envelope_abundance_decay(self, name, intershell, initial=None):
+        # INFO: gets the initial envelope abundance of the element
+        # scaled by the metallicity of the model.
+
+        # INFO: THIS is the naive method that just uses a scaled metallicity
+
+        # initial_envelope_abundance = self.df.get_initial_envelope_abundance(
+        #     element=name,
+        #     metallicity=self.Z,
+        # )
+
+        # INFO: THIS is the linearly interpolated method
+        if initial == None:
+            if type(self.initial_abundance) == type(None):
+                initial_envelope_abundance = self.initial_envelope_abundances[
+                    self.initial_envelope_abundances["element"] == name
+                ]["massfrac"]
+            else:
+                initial_envelope_abundance = self.initial_abundance
+        else:
+            initial_envelope_abundance = initial
+
+        # INFO: computes the elemental abundance in the envelope by
+        # enriching it with intershell abundances.
+        envelope = np.zeros(self.total_length)
+        delta_M_element = intershell * self.m_dup
+        dt = np.concatenate([[0], np.diff(self.time)])
+        t_half = 1.61e6
+        lam = np.log(2) / t_half
+
+        for i in range(self.total_length):
+            if i == 0:
+                envelope[i] = initial_envelope_abundance
+                continue
+
+            # INFO: this is WRONG because the envelope mass is taken AFTER dredge-up
+            # already occurred.
+
+            # envelope[i] = (envelope[i - 1] * self.m_env[i] + delta_M_element[i]) / (
+            #     self.m_env[i] + self.m_dup[i]
+            # )
+
+            envelope[i] = (
+                envelope[i - 1] * (self.m_env[i] - self.m_dup[i]) + delta_M_element[i]
+            ) / self.m_env[i]
+
+            envelope[i] *= np.exp(-lam * dt[i])
 
         return envelope
 
--- scripts/w28/what-wrong-with-the-yields.py
+++ scripts/w28/what-wrong-with-the-yields.py
@@ -677,4 +677,332 @@ def dNdt(lam, N):
     return -lam * N
 
 
-N = N + dNdt(lam, N) * dt
+dt = 1000
+N = 0
+t_half = 1.61e6
+lam = np.log(2) / t_half
+
+ns = [N]
+ts = [0]
+for i in range(5000):
+    N = N + dNdt(lam, N) * dt
+    if i % 10 == 0:
+        ts.append(ts[-1] + 10 * dt)
+        ns.append(N)
+
+    if i % 100 == 0 and i != 0:
+        N += 0.01e6
+
+plt.plot(ts[:-1], ns[1:])
+i = 1e6
+t_half = 1.61e6
+
+dt = 1000
+N = 0
+
+ns = [N]
+ts = [0]
+for i in range(5000):
+    if i % 10 == 0:
+        ts.append(ts[-1] + 10 * dt)
+        ns.append(N)
+
+    if i % 100 == 0 and i != 0:
+        N += 0.01e6
+
+plt.plot(ts[:-1], ns[1:])
+i = 1e6
+t_half = 1.61e6
+
+
+plt.show()
+# %%
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(column, height=0.8), constrained_layout=True
+)
+
+ab = Abundances(None, df, method="tp offset", mass=m)
+star = get_star(m=m)
+final = []
+z = []
+els = []
+
+star = get_star(m=m)
+
+plt.plot(
+    ab.time[star.ntpagb :] - ab.time[star.ntpagb],
+    ab.zr93.intershell[star.ntpagb :],
+    label="Intershell",
+    linewidth=1,
+)
+plt.plot(
+    ab.time[star.ntpagb :] - ab.time[star.ntpagb],
+    ab.zr93.envelope[star.ntpagb :],
+    label="Envelope without decay",
+    linewidth=1,
+)
+plt.plot(
+    ab.time[star.ntpagb :] - ab.time[star.ntpagb],
+    ab.zr93.envelope_decay[star.ntpagb :],
+    label="Envelope with decay",
+    linewidth=1,
+)
+
+plt.plot(
+    ab.time[star.ntpagb :] - ab.time[star.ntpagb],
+    ab.zr93.envelope[star.ntpagb :] - ab.zr93.envelope_decay[star.ntpagb :],
+    label="Decayed enevelope",
+    linewidth=1,
+)
+
+plt.yscale("log")
+plt.ylim(1e-10)
+plt.xlabel("TPAGB age (yr)")
+plt.ylabel("$X(^{93}\\textrm{Zr})$")
+
+fig.legend(loc="outside upper center", ncols=2)
+
+axs.spines[["right", "top"]].set_visible(False)
+plt.savefig("/home/koen/LaTeX-setup/plots/w28-envelope-decay.pgf", format="pgf")
+plt.show()
+plt.close()
+
+# %%
+
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(column), constrained_layout=True
+)
+
+for m in np.arange(2.5, 2.501, 0.1):
+    ab = Abundances(None, df, method="tp offset", mass=m)
+    star = get_star(m=m)
+    final = []
+    z = []
+    els = []
+    plt.plot(
+        ab.tp_count[star.ntpagb :],
+        ab.nb.intershell[star.ntpagb :],
+        c="C0",
+        linewidth=3,
+        alpha=0.5,
+        label="MESA intershell",
+    )
+    decay = ab.zr93.envelope[star.ntpagb :] - ab.zr93.envelope_decay[star.ntpagb :]
+
+    plt.plot(
+        ab.tp_count[star.ntpagb :],
+        ab.nb.envelope[star.ntpagb :] + decay,
+        c="C0",
+        label="MESA envelope",
+    )
+    # plt.plot(ab.tp_count[star.ntpagb:], ab.nb.m_yield[star.ntpagb:])
+
+with open("data/intershell_pd_df.pkl", "rb") as f:
+    intershell = pickle.load(f)
+
+filtered = intershell.query("M1tp==2.5 and Z == 0.007 and last == 1")
+plt.plot(
+    filtered.ntp,
+    93 * filtered.nb93 + 94 * filtered.nb94 + 95 * filtered.nb95,
+    c="C1",
+    linewidth=3,
+    alpha=0.5,
+    label="Monash intershell $z=0.007$",
+)
+
+with open("data/env_pd_df.pkl", "rb") as f:
+    envelope = pickle.load(f)
+
+filtered = envelope.query(
+    "M_init == 2.5 and N_ov != 0 and element == 'nb' and Z == 0.007"
+)
+plt.plot(filtered["ntp"], filtered.massfrac, c="C1", label="Monash envelope $z=0.007$")
+
+with open("data/intershell_pd_df.pkl", "rb") as f:
+    intershell = pickle.load(f)
+
+filtered = intershell.query("M1tp==2.5 and Z == 0.0028 and last == 1")
+plt.plot(
+    filtered.ntp,
+    93 * filtered.nb93 + 94 * filtered.nb94 + 95 * filtered.nb95,
+    c="C2",
+    linewidth=3,
+    alpha=0.5,
+    label="Monash intershell $z=0.0028$",
+)
+
+with open("data/env_pd_df.pkl", "rb") as f:
+    envelope = pickle.load(f)
+
+filtered = envelope.query(
+    "M_init == 2.5 and N_ov != 0 and element == 'nb' and Z == 0.0028 and pmz==0.002"
+)
+plt.plot(filtered["ntp"], filtered.massfrac, c="C2", label="Monash envelope $z=0.0028$")
+
+plt.yscale("log")
+
+plt.text(
+    0.05, 0.95, "$M = 2.5\\;M_\\odot$", transform=axs.transAxes, va="top", ha="left"
+)
+
+fig.legend(loc="outside upper center", ncols=2)
+
+axs.spines[["right", "top"]].set_visible(False)
+plt.xlabel("Thermal pulse count")
+plt.ylabel("$X(\\textrm{Nb})$")
+plt.savefig("/home/koen/LaTeX-setup/plots/w28-fix-nb-2.pgf", format="pgf")
+plt.show()
+plt.close()
+# %%
+
+import matplotlib.transforms as mtransforms
+
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(full), constrained_layout=True
+)
+
+norm = plt.Normalize(1, 3)
+cmap = plt.cm.viridis
+
+m_yield = yields.query("`Initial mass` == 2.5 and metallicity == 0.007")
+
+for m in np.arange(2.5, 2.501, 0.1):
+    ab = Abundances(None, df, method="tp offset", mass=m)
+    star = get_star(m=m)
+    final = []
+    z = []
+    els = []
+    final.append(ab.h.m_yield[-1])
+    final.append(ab.he.m_yield[-1])
+    z.append(1)
+    z.append(2)
+    els.append("h")
+    els.append("he")
+    for el in list(ab.df.elements)[7:]:
+        if el in ["tc", "pm", "po"]:
+            continue
+
+        if el in ["nb"]:
+            e = ab.__getattr__(ab.df.elements[el].key).m_yield[-1]
+            print(e)
+            x = ab.zr93.envelope - ab.zr93.envelope_decay
+            e += 2 * np.cumsum(x * ab.dm)[-1]
+            print(e)
+
+        elif el in ["zr"]:
+            e = ab.__getattr__(ab.df.elements[el].key).m_yield[-1]
+            x = ab.zr93.envelope - ab.zr93.envelope_decay
+            e -= 2 * np.cumsum(x * ab.dm)[-1]
+        else:
+            e = ab.__getattr__(ab.df.elements[el].key).m_yield[-1]
+
+        final.append(e)
+        data = ab.df.envelope[ab.df.envelope["element"] == el]
+        z.append(int(np.array(data["elemental_mass"])[0]))
+        # z.append(ab.df.envelope.query(f"element == {el}")["elemental_mass"])
+        els.append(el)
+
+    plt.plot(
+        z,
+        np.array(final) / np.array(m_yield["Mass(i)"]),
+        label=f"MESA $M={m:.1f}\\;M_\\odot,\\;Z=0.00557$",
+        linewidth=1,
+    )
+
+
+m_yield_new = yields.query(
+    "`Initial mass` == 2.5 and metallicity == 0.0028 and M_mix == 0.002"
+)
+
+# plt.plot(m_yield["El"], m_yield["Mass(i)"])
+plt.plot(
+    m_yield["Z"],
+    np.array(m_yield_new["Mass(i)"]) / np.array(m_yield["Mass(i)"]),
+    label="$M=2.5\\;M_\\odot,\\;Z=0.0028,\\;M_\\textrm{mix} = 0.002$",
+    linewidth=1,
+)
+
+
+m_yield_new = yields.query(
+    "`Initial mass` == 2.5 and metallicity == 0.0028 and M_mix == 0.004"
+)
+
+# plt.plot(m_yield["El"], m_yield["Mass(i)"])
+plt.plot(
+    m_yield["Z"],
+    np.array(m_yield_new["Mass(i)"]) / np.array(m_yield["Mass(i)"]),
+    label="$M=2.5\\;M_\\odot,\\;Z=0.0028,\\;M_\\textrm{mix} = 0.004$",
+    linewidth=1,
+)
+
+m_yield_new = yields.query("`Initial mass` == 2.5 and metallicity == 0.007")
+# plt.plot(m_yield["El"], m_yield["Mass(i)"])
+plt.plot(
+    m_yield["Z"],
+    np.array(m_yield_new["Mass(i)"]) / np.array(m_yield["Mass(i)"]),
+    label="$M=2.5\\;M_\\odot,\\;Z=0.007$",
+    linewidth=1,
+)
+
+fig.legend(loc="outside upper center", ncols=2)
+
+labels = els
+labels = []
+for el in els:
+    labels.append(el.capitalize())
+
+plt.xlabel("Element")
+plt.xticks(z[::2], labels=labels[::2])
+
+ax_t = axs.secondary_xaxis("top")
+ax_t.set_xticks(z[1::2])
+ax_t.set_xticklabels(labels[1::2])
+
+for label in axs.get_xticklabels():
+    label.set_verticalalignment("baseline")
+
+for label in ax_t.get_xticklabels():
+    label.set_verticalalignment("baseline")
+
+for i, label in enumerate(axs.get_xticklabels()):
+    label.set_y(-0.00 if i % 2 == 0 else -0.03)
+
+
+for i, tick in enumerate(ax_t.xaxis.get_major_ticks()):
+    tick.tick2line.set_markersize(7 if i % 2 else 0)
+
+    label = tick.label2
+
+    offset = 10 if i % 2 else 3
+
+    label.set_transform(
+        ax_t.get_xaxis_transform()
+        + mtransforms.ScaledTranslation(
+            0,
+            offset / 72,
+            fig.dpi_scale_trans,
+        )
+    )
+
+
+ticks = axs.xaxis.get_major_ticks()
+
+for i, tick in enumerate(ticks):
+    length = 7 if i % 2 else 0
+
+    tick.tick1line.set_markersize(length)
+    tick.tick2line.set_markersize(length)
+
+for i in z[::2]:
+    axs.axvline(i, color="C9", linewidth=0.75 / 2, zorder=-1)
+
+
+plt.ylim(0.1, 10)
+plt.ylabel("$M_X / M_{X,M=2.5\\;M_\\odot,\\;Z=0.007}$")
+plt.yscale("log")
+plt.savefig(
+    "/home/koen/LaTeX-setup/plots/w28-yield-all-diff-m2.5-fix-nb-2.pgf", format="pgf"
+)
+plt.show()
+plt.close()
+# %%
