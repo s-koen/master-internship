c2f80019283aa5597f899c42445cd8128a852180
kessers <koen.essers@ru.nl>
2026-09-15 22:59:11 +0200

    added cumulative mass plot

--- scripts/w28/what-wrong-with-the-yields.py
+++ scripts/w28/what-wrong-with-the-yields.py
@@ -885,14 +885,14 @@ for m in np.arange(2.5, 2.501, 0.1):
         if el in ["nb"]:
             e = ab.__getattr__(ab.df.elements[el].key).m_yield[-1]
             print(e)
-            x = ab.zr93.envelope - ab.zr93.envelope_decay
-            e += 2 * np.cumsum(x * ab.dm)[-1]
+            x = ab.zr93.envelope
+            e += np.cumsum(x * ab.dm)[-1]
             print(e)
 
         elif el in ["zr"]:
             e = ab.__getattr__(ab.df.elements[el].key).m_yield[-1]
-            x = ab.zr93.envelope - ab.zr93.envelope_decay
-            e -= 2 * np.cumsum(x * ab.dm)[-1]
+            x = ab.zr93.envelope
+            e -= np.cumsum(x * ab.dm)[-1]
         else:
             e = ab.__getattr__(ab.df.elements[el].key).m_yield[-1]
 
@@ -1001,8 +1001,270 @@ plt.ylim(0.1, 10)
 plt.ylabel("$M_X / M_{X,M=2.5\\;M_\\odot,\\;Z=0.007}$")
 plt.yscale("log")
 plt.savefig(
-    "/home/koen/LaTeX-setup/plots/w28-yield-all-diff-m2.5-fix-nb-2.pgf", format="pgf"
+    "/home/koen/LaTeX-setup/plots/w28-yield-all-diff-m2.5-fix-nb-3.pgf", format="pgf"
 )
 plt.show()
 plt.close()
 # %%
+
+import matplotlib.transforms as mtransforms
+
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(full), constrained_layout=True
+)
+
+norm = plt.Normalize(1, 3)
+cmap = plt.cm.viridis
+
+m_yield = yields.query("`Initial mass` == 2.5 and metallicity == 0.007")
+
+for m in np.arange(2.5, 2.501, 0.1):
+    ab = Abundances(None, df, method="tp offset", mass=m)
+    star = get_star(m=m)
+    final = []
+    z = []
+    els = []
+    final.append(ab.h.m_yield[-1])
+    final.append(ab.he.m_yield[-1])
+    z.append(1)
+    z.append(2)
+    els.append("h")
+    els.append("he")
+    for el in list(ab.df.elements)[7:]:
+        if el in ["tc", "pm", "po"]:
+            continue
+        e = ab.__getattr__(ab.df.elements[el].key).m_yield[-1]
+        final.append(e)
+        data = ab.df.envelope[ab.df.envelope["element"] == el]
+        z.append(int(np.array(data["elemental_mass"])[0]))
+        # z.append(ab.df.envelope.query(f"element == {el}")["elemental_mass"])
+        els.append(el)
+
+    plt.plot(
+        z,
+        np.cumsum(np.array(final)),
+        label=f"MESA $M={m:.1f}\\;M_\\odot,\\;Z=0.00557$",
+        linewidth=1,
+    )
+
+
+m_yield_new = yields.query(
+    "`Initial mass` == 2.5 and metallicity == 0.0028 and M_mix == 0.002"
+)
+
+# plt.plot(m_yield["El"], m_yield["Mass(i)"])
+plt.plot(
+    m_yield["Z"],
+    np.cumsum(np.array(m_yield_new["Mass(i)"])),
+    label="$M=2.5\\;M_\\odot,\\;Z=0.0028,\\;M_\\textrm{mix} = 0.002$",
+    linewidth=1,
+)
+
+
+m_yield_new = yields.query("`Initial mass` == 2.5 and metallicity == 0.007")
+# plt.plot(m_yield["El"], m_yield["Mass(i)"])
+plt.plot(
+    m_yield["Z"],
+    np.cumsum(np.array(m_yield_new["Mass(i)"])),
+    label="$M=2.5\\;M_\\odot,\\;Z=0.007$",
+    linewidth=1,
+)
+
+
+fig.legend(loc="outside upper center", ncols=2)
+
+labels = els
+labels = []
+for el in els:
+    labels.append(el.capitalize())
+
+plt.xlabel("Element")
+plt.xticks(z[::2], labels=labels[::2])
+
+ax_t = axs.secondary_xaxis("top")
+ax_t.set_xticks(z[1::2])
+ax_t.set_xticklabels(labels[1::2])
+
+for label in axs.get_xticklabels():
+    label.set_verticalalignment("baseline")
+
+for label in ax_t.get_xticklabels():
+    label.set_verticalalignment("baseline")
+
+for i, label in enumerate(axs.get_xticklabels()):
+    label.set_y(-0.00 if i % 2 == 0 else -0.03)
+
+
+for i, tick in enumerate(ax_t.xaxis.get_major_ticks()):
+    tick.tick2line.set_markersize(7 if i % 2 else 0)
+
+    label = tick.label2
+
+    offset = 10 if i % 2 else 3
+
+    label.set_transform(
+        ax_t.get_xaxis_transform()
+        + mtransforms.ScaledTranslation(
+            0,
+            offset / 72,
+            fig.dpi_scale_trans,
+        )
+    )
+
+
+ticks = axs.xaxis.get_major_ticks()
+
+for i, tick in enumerate(ticks):
+    length = 7 if i % 2 else 0
+
+    tick.tick1line.set_markersize(length)
+    tick.tick2line.set_markersize(length)
+
+for i in z[::2]:
+    axs.axvline(i, color="C9", linewidth=0.75 / 2, zorder=-1)
+
+
+# plt.ylim(0.1, 10)
+plt.ylabel("$\sum M_X$ ($M_\\odot$)")
+# plt.yscale("log")
+plt.savefig("/home/koen/LaTeX-setup/plots/w28-yield-cum.pgf", format="pgf")
+plt.show()
+plt.close()
+# %%
+import matplotlib.transforms as mtransforms
+
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(full), constrained_layout=True
+)
+
+norm = plt.Normalize(1, 3)
+cmap = plt.cm.viridis
+
+m_yield = yields.query("`Initial mass` == 2.5 and metallicity == 0.007")
+
+for m in np.arange(2.5, 2.501, 0.1):
+    ab = Abundances(None, df, method="tp offset", mass=m)
+    star = get_star(m=m)
+    final = []
+    z = []
+    els = []
+    final.append(ab.h.m_yield[-1])
+    final.append(ab.he.m_yield[-1])
+    z.append(1)
+    z.append(2)
+    els.append("h")
+    els.append("he")
+    for el in list(ab.df.elements)[7:]:
+        if el in ["tc", "pm", "po"]:
+            continue
+        e = ab.__getattr__(ab.df.elements[el].key).m_yield[-1]
+        final.append(e)
+        data = ab.df.envelope[ab.df.envelope["element"] == el]
+        z.append(int(np.array(data["elemental_mass"])[0]))
+        # z.append(ab.df.envelope.query(f"element == {el}")["elemental_mass"])
+        els.append(el)
+
+    plt.plot(
+        z,
+        (np.array(final) / np.sum(final))
+        / (np.array(m_yield["Mass(i)"]) / np.sum(np.array(m_yield["Mass(i)"]))),
+        label=f"MESA $M={m:.1f}\\;M_\\odot,\\;Z=0.00557$",
+        linewidth=1,
+    )
+
+
+m_yield_new = yields.query(
+    "`Initial mass` == 2.5 and metallicity == 0.0028 and M_mix == 0.002"
+)
+
+# plt.plot(m_yield["El"], m_yield["Mass(i)"])
+plt.plot(
+    m_yield["Z"],
+    (np.array(m_yield_new["Mass(i)"]) / np.sum(np.array(m_yield_new["Mass(i)"])))
+    / (np.array(m_yield["Mass(i)"]) / np.sum(np.array(m_yield["Mass(i)"]))),
+    label="$M=2.5\\;M_\\odot,\\;Z=0.0028,\\;M_\\textrm{mix} = 0.002$",
+    linewidth=1,
+)
+
+
+m_yield_new = yields.query(
+    "`Initial mass` == 2.5 and metallicity == 0.0028 and M_mix == 0.004"
+)
+
+# plt.plot(m_yield["El"], m_yield["Mass(i)"])
+plt.plot(
+    m_yield["Z"],
+    (np.array(m_yield_new["Mass(i)"]) / np.sum(np.array(m_yield_new["Mass(i)"])))
+    / (np.array(m_yield["Mass(i)"]) / np.sum(np.array(m_yield["Mass(i)"]))),
+    label="$M=2.5\\;M_\\odot,\\;Z=0.0028,\\;M_\\textrm{mix} = 0.004$",
+    linewidth=1,
+)
+
+m_yield_new = yields.query("`Initial mass` == 2.5 and metallicity == 0.007")
+# plt.plot(m_yield["El"], m_yield["Mass(i)"])
+plt.plot(
+    m_yield["Z"],
+    np.array(m_yield_new["Mass(i)"]) / np.array(m_yield["Mass(i)"]),
+    label="$M=2.5\\;M_\\odot,\\;Z=0.007$",
+    linewidth=1,
+)
+
+fig.legend(loc="outside upper center", ncols=2)
+
+labels = els
+labels = []
+for el in els:
+    labels.append(el.capitalize())
+
+plt.xlabel("Element")
+plt.xticks(z[::2], labels=labels[::2])
+
+ax_t = axs.secondary_xaxis("top")
+ax_t.set_xticks(z[1::2])
+ax_t.set_xticklabels(labels[1::2])
+
+for label in axs.get_xticklabels():
+    label.set_verticalalignment("baseline")
+
+for label in ax_t.get_xticklabels():
+    label.set_verticalalignment("baseline")
+
+for i, label in enumerate(axs.get_xticklabels()):
+    label.set_y(-0.00 if i % 2 == 0 else -0.03)
+
+
+for i, tick in enumerate(ax_t.xaxis.get_major_ticks()):
+    tick.tick2line.set_markersize(7 if i % 2 else 0)
+
+    label = tick.label2
+
+    offset = 10 if i % 2 else 3
+
+    label.set_transform(
+        ax_t.get_xaxis_transform()
+        + mtransforms.ScaledTranslation(
+            0,
+            offset / 72,
+            fig.dpi_scale_trans,
+        )
+    )
+
+
+ticks = axs.xaxis.get_major_ticks()
+
+for i, tick in enumerate(ticks):
+    length = 7 if i % 2 else 0
+
+    tick.tick1line.set_markersize(length)
+    tick.tick2line.set_markersize(length)
+
+for i in z[::2]:
+    axs.axvline(i, color="C9", linewidth=0.75 / 2, zorder=-1)
+
+
+plt.ylim(0.1, 10)
+plt.ylabel("$M_X / M_{X,M=2.5\\;M_\\odot,\\;Z=0.007}$")
+plt.yscale("log")
+plt.savefig("/home/koen/LaTeX-setup/plots/w28-yield-mass-correction.pgf", format="pgf")
+plt.show()
+plt.close()
