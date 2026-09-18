40cfa7e0486453153ec69595cc5566c1e1158789
kessers <koen.essers@ru.nl>
2026-09-19 01:37:11 +0200

    made plot wider

--- scripts/w28/accretion-properties.py
+++ scripts/w28/accretion-properties.py
@@ -67,7 +67,7 @@ fig, axs = plt.subplots(
     2,
     sharey=True,
     sharex=True,
-    figsize=set_size(column),
+    figsize=set_size(full),
     constrained_layout=True,
 )
 
@@ -102,7 +102,7 @@ for i, ax in enumerate(axs):
                 ax.text(
                     r,
                     qq,
-                    f"{ratio[k, j]:.1%}\\%",
+                    f"{ratio[k, j]:.1%}",
                     ha="center",
                     va="center",
                     fontsize=6,
@@ -137,6 +137,228 @@ plt.yticks(
 )
 
 plt.savefig("/home/koen/LaTeX-setup/plots/w28-accretion-1.pgf", format="pgf")
+plt.show()
+plt.close()
+
+
+# %%
+
+df = AbundanceTables()
+
+
+def z(r):
+    print(r)
+    try:
+        r.period_days[-1]
+    except IndexError:
+        return np.nan
+
+    if r.envelope_mass[-1] > 1e-2:
+        return np.nan
+
+    # ab = Abundances(model=r, df=df)
+    # return np.log10(ab.ba.envelope[-1])
+
+    return np.max(r.R / r.rl_1)
+    # return r.period_days[-1]
+
+
+fig, axs = plt.subplots(
+    2,
+    2,
+    sharey=True,
+    sharex=True,
+    figsize=set_size(full),
+    constrained_layout=True,
+)
+
+axs = axs.flatten()
+
+minn = 1e99
+maxx = -1e99
+
+
+# R, q, ratio = grid.array(z, x="R", y="q")
+
+
+ms = [1.8, 2.2, 2.6, 3.0]
+
+ratios = []
+for m in ms:
+    R, q, ratio = grid.array(z, x="R", y="q", m=m)
+    ratios.append(ratio)
+
+
+for ratio in ratios:
+    minn = np.nanmin(ratio) if np.nanmin(ratio) < minn else minn
+    maxx = np.nanmax(ratio) if np.nanmax(ratio) > maxx else maxx
+
+from matplotlib.colors import TwoSlopeNorm
+
+norm = TwoSlopeNorm(vmin=minn, vcenter=1, vmax=maxx)
+
+for i, ax in enumerate(axs):
+
+    ax.set_title(f"$M_\\textrm{{TPAGB}} = {ms[i]:.1f}\\;M_\\odot$")
+    ratio = ratios[i]
+    for k, r in enumerate(R):
+        for j, qq in enumerate(q):
+            try:
+                ax.text(
+                    r,
+                    qq,
+                    f"{ratio[k, j]:.2f}",
+                    ha="center",
+                    va="center",
+                    fontsize=6,
+                    c="k" if (ratio[k, j] - minn) / (maxx - minn) > 0.5 else "w",
+                )
+            except:
+                pass
+
+    c = ax.pcolormesh(
+        R,
+        q,
+        ratio.T,
+        shading="auto",
+        cmap="coolwarm",
+        norm=norm,
+        rasterized=True,
+    )
+
+
+fig.supxlabel(r"Roche lobe radius ($R_\odot$)", fontsize=10)
+fig.supylabel("$q$ ($M_\\textrm{a} / M_\\textrm{d}$)", fontsize=10)
+cbar = plt.colorbar(
+    c,
+    ax=axs,
+    orientation="vertical",
+    location="right",
+    label=r"$\textrm{max}(R_\textrm{star} / R_\textrm{rl})$",
+)
+
+cbar.ax.set_yscale("linear")
+
+plt.yticks(
+    [0.7, 0.66, 0.63, 0.6, 0.5, 0.44, 0.4][::-1], [f"{q:.2f}" for q in grid.axes["q"]]
+)
+
+plt.savefig("/home/koen/LaTeX-setup/plots/w28-accretion-2.pgf", format="pgf")
+plt.show()
+plt.close()
+
+
+# %%
+
+
+def rol(history):
+    q = history.star_2_mass / history.star_1_mass
+    return history.rl_1 * (1 + (0.441 * q ** (-0.325)) / (1 + 0.412 * q ** (-0.8)))
+
+
+# %%
+
+df = AbundanceTables()
+
+
+def z(r):
+    print(r)
+    try:
+        r.period_days[-1]
+    except IndexError:
+        return np.nan
+
+    if r.envelope_mass[-1] > 1e-2:
+        return np.nan
+
+    # ab = Abundances(model=r, df=df)
+    # return np.log10(ab.ba.envelope[-1])
+
+    return np.max(r.R / rol(r))
+    # return r.period_days[-1]
+
+
+fig, axs = plt.subplots(
+    2,
+    2,
+    sharey=True,
+    sharex=True,
+    figsize=set_size(full),
+    constrained_layout=True,
+)
+
+axs = axs.flatten()
+
+minn = 1e99
+maxx = -1e99
+
+
+# R, q, ratio = grid.array(z, x="R", y="q")
+
+
+ms = [1.8, 2.2, 2.6, 3.0]
+
+ratios = []
+for m in ms:
+    R, q, ratio = grid.array(z, x="R", y="q", m=m)
+    ratios.append(ratio)
+
+
+for ratio in ratios:
+    minn = np.nanmin(ratio) if np.nanmin(ratio) < minn else minn
+    maxx = np.nanmax(ratio) if np.nanmax(ratio) > maxx else maxx
+
+from matplotlib.colors import TwoSlopeNorm
+
+norm = TwoSlopeNorm(vmin=minn, vcenter=1, vmax=maxx)
+
+for i, ax in enumerate(axs):
+
+    ax.set_title(f"$M_\\textrm{{TPAGB}} = {ms[i]:.1f}\\;M_\\odot$")
+    ratio = ratios[i]
+    for k, r in enumerate(R):
+        for j, qq in enumerate(q):
+            try:
+                ax.text(
+                    r,
+                    qq,
+                    f"{ratio[k, j]:.2f}",
+                    ha="center",
+                    va="center",
+                    fontsize=6,
+                    c="k" if (ratio[k, j] - minn) / (maxx - minn) > 0.5 else "w",
+                )
+            except:
+                pass
+
+    c = ax.pcolormesh(
+        R,
+        q,
+        ratio.T,
+        shading="auto",
+        cmap="coolwarm",
+        norm=norm,
+        rasterized=True,
+    )
+
+
+fig.supxlabel(r"Roche lobe radius ($R_\odot$)", fontsize=10)
+fig.supylabel("$q$ ($M_\\textrm{a} / M_\\textrm{d}$)", fontsize=10)
+cbar = plt.colorbar(
+    c,
+    ax=axs,
+    orientation="vertical",
+    location="right",
+    label=r"$\textrm{max}(R_\textrm{star} / R_\textrm{rol})$",
+)
+
+cbar.ax.set_yscale("linear")
+
+plt.yticks(
+    [0.7, 0.66, 0.63, 0.6, 0.5, 0.44, 0.4][::-1], [f"{q:.2f}" for q in grid.axes["q"]]
+)
+
+plt.savefig("/home/koen/LaTeX-setup/plots/w28-accretion-3.pgf", format="pgf")
 plt.show()
 plt.close()
 
