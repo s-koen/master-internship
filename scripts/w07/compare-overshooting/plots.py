f06261010a576cdde7dc379781cf6bc86584ed44
kessers <koen.essers@ru.nl>
2026-09-19 01:36:46 +0200

    testing 5 solar mass kippenhahn plots of TPAGB phase

--- scripts/w07/compare-overshooting/plots.py
+++ scripts/w07/compare-overshooting/plots.py
@@ -837,3 +837,148 @@ plt.close()
 
 print(eep.abun)
 # %%
+
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(column), constrained_layout=True
+)
+
+
+kipp_args = mkipp.Kipp_Args(
+    logs_dirs=[
+        f"/home/koen/master-internship/mesa-models/compare-overshooting/rees/{5}msun/LOGS/EAGB/"
+    ],
+    xaxis="star_age",
+    save_file=False,
+    decorate_plot=False,
+    # contour_colormap=plt.get_cmap("Greens"),
+    levels=np.linspace(-1, 5, 50),
+)
+mkipp.kipp_plot(kipp_args, axis=axs)
+plt.show()
+
+# %%
+
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(column), constrained_layout=True
+)
+
+
+kipp_args = mkipp.Kipp_Args(
+    logs_dirs=[f"/home/koen/master-internship/mesa-models/{5}msun/LOGS/TPAGB/"],
+    xaxis="star_age",
+    save_file=False,
+    decorate_plot=False,
+    # contour_colormap=plt.get_cmap("Greens"),
+    # levels=np.linspace(-1, 5, 50),
+    log10_on_data=True,
+    levels=np.linspace(-1, 7, 50),
+    identifier="tri_alpha",
+)
+mkipp.kipp_plot(kipp_args, axis=axs)
+plt.show()
+
+
+# %%
+
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(column), constrained_layout=True
+)
+
+
+kipp_args = mkipp.Kipp_Args(
+    logs_dirs=[f"/home/koen/master-internship/mesa-models/{5}msun/LOGS/TPAGB/"],
+    xaxis="star_age",
+    save_file=False,
+    decorate_plot=False,
+    # contour_colormap=plt.get_cmap("Greens"),
+    # levels=np.linspace(-1, 5, 50),
+    log10_on_data=True,
+    levels=np.arange(-5, 1.01, 0.1),
+    identifier="z_mass_fraction_metals",
+)
+mkipp.kipp_plot(kipp_args, axis=axs)
+plt.show()
+
+
+# %%
+
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(column), constrained_layout=True
+)
+
+
+kipp_args = mkipp.Kipp_Args(
+    logs_dirs=[f"/home/koen/master-internship/mesa-models/{5}msun/LOGS/TPAGB/"],
+    xaxis="star_age",
+    save_file=False,
+    decorate_plot=False,
+    # contour_colormap=plt.get_cmap("Greens"),
+    # levels=np.linspace(-1, 5, 50),
+    log10_on_data=True,
+    levels=np.linspace(-100, 50, 50),
+    identifier="tri_alpha",
+)
+mkipp.kipp_plot(kipp_args, axis=axs)
+plt.show()
+
+
+# %%
+
+fig, axs = plt.subplots(
+    1, 1, sharex=True, figsize=set_size(column), constrained_layout=True
+)
+
+
+kipp_args = mkipp.Kipp_Args(
+    logs_dirs=[
+        f"/home/koen/master-internship/mesa-models/tpagb-test-high-mass/LOGS/TPAGB"
+    ],
+    xaxis="star_age",
+    save_file=False,
+    decorate_plot=False,
+    # contour_colormap=plt.get_cmap("Greens"),
+    # levels=np.linspace(-1, 5, 50),
+    log10_on_data=True,
+    levels=np.linspace(-100, 50, 50),
+    identifier="tri_alpha",
+)
+mkipp.kipp_plot(kipp_args, axis=axs)
+plt.show()
+
+
+# %%
+
+data = mr.MesaData(
+    f"/home/koen/master-internship/mesa-models/tpagb-test-high-mass/LOGS/TPAGB/history.data"
+)
+# %%
+
+data.bulk_names
+
+# %%
+
+datas = []
+
+for phase in ["MS", "GB", "CHeB", "EAGB", "TPAGB"]:
+    datas.append(
+        mr.MesaData(
+            f"/home/koen/master-internship/mesa-models/tpagb-test-high-mass/LOGS/{phase}/history.data"
+        )
+    )
+
+
+# %%
+
+for data in datas:
+    plt.plot(data.log_cntr_Rho, data.log_cntr_T)
+
+plt.show()
+# %%
+
+plt.plot(data.star_age, data.log_LHe)
+plt.plot(data.star_age, data.log_L)
+plt.show()
+# %%
+
+print(data.model_number[-1])
+# %%
